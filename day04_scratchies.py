cbc079fd79fcba37a5be3d85540fa611d2d6ecd7
Craig Penfold <craig.penfold@gmail.com>
2023-12-11 17:07:28 +1100

    adding part one for day 4

---
+++ day04_scratchies.py
@@ -0,0 +1,21 @@
+import re
+
+input_file = open("day04_input.txt", 'r');
+
+game_count = 0
+total_score = 0
+
+for input_line in input_file:
+    scratchie_game = re.split(r': |\|', input_line.strip())
+    for winning_number in scratchie_game[1].split():
+        if winning_number in scratchie_game[2].split():
+            #count = 1 if count == 0 else count = count * 2
+            if game_count==0:
+                game_count = 1
+            else:
+                game_count = game_count * 2
+    total_score += game_count
+    #reset counter
+    game_count = 0
+
+print(total_score)
